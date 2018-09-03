ee52ce39f1c2619fbfe6b20f067b7c7408587fab
Manoj kumar.R <rmanojkumar23@gmail.com>
2018-09-03 14:05:53 +0530

    Create step14.java

---
+++ step14.java
@@ -0,0 +1,10 @@
+class s9 {
+	public static void main(String args[]) {
+		System.out.println("The Odd Numbers are:");
+		for (int i = 2; i <= 6; i++) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
